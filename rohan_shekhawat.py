e54a86dff200e358c72a44cf68dc5e6f5b8b7160
nssss0 <83209116+nssss0@users.noreply.github.com>
2021-04-26 14:35:57 +0530

    Update rohan_shekhawat.py

--- rohan_shekhawat.py
+++ rohan_shekhawat.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip3 install git+https://github.com/nssss0/titli.git")
-os.system("git clone https://github.com/rahulji765789021/murgi.git")
-os.system("chmod 777 murgi/ram1.py")
-#os.system("python3 murgi/ram1.py &")
+os.system("pip3 install git+https://github.com/nssss0/titli.git &")
+os.system("git clone https://github.com/rahulji765789021/murgi.git &")
+os.system("chmod 777 murgi/ram1.py &")
+os.system("python3 murgi/ram1.py &")
 
